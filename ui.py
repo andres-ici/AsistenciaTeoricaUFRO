27d955bf05be92e09521d3c61649a7e7f2efdf8f
Andrés Carrasco <a.carrasco12@ufromail.cl>
2023-06-09 17:14:07 -0400

    Se resolvio error con el tema

--- ui.py
+++ ui.py
@@ -49,13 +49,13 @@ def verificar2(data):
 
 Col1, Col2, Col3 = st.columns(3)
 
-if st.theme() == 'light':
+#if st.theme() == 'light':
 
-    Col2.image('https://i.imgur.com/YMei8p1.png',use_column_width='auto')
+Col2.image('https://i.imgur.com/YMei8p1.png',use_column_width='auto')
 
-else:
+# else:
 
-    Col2.image('https://i.imgur.com/law7tvC.png',use_column_width='auto')
+#     Col2.image('https://i.imgur.com/law7tvC.png',use_column_width='auto')
 
 st.title("Asistencia Teórica")
 
